313162c78a2cebb3d9bbe104151443be0b4a2222
Russell Hickey <RHickeyJnr@gmail.com>
2016-08-13 22:22:14 +0100

    Update from repo

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,18 +1,34 @@
-(function(R) {   
-    
+(function(R) {
+
     function dragStart(x, y, e) {
+        this.animating = true;
         this.current_transform = this.transform();
     }
-    
+
     function dragMove(dx, dy, x, y, e) {
         this.transform(this.current_transform+'T'+dx+','+dy)
             .updatePaths();
     }
-    
+
     function dragEnd(e) {
+        var that = this;
+
+        this.animate({
+          transform: this.original_transform
+        }, 500, 'bounce', function() {
+           that.animating = false;
+        });
+
+        var animation = setInterval(function() {
+            that.updatePaths();
+            if(!that.animating) {
+                clearTimeout(animation);
+            }
+        }, 8);
+
         this.current_transform = this.transform();
     }
-    
+
     function getPathString(obj1, obj2) {
         var w1 = obj1.node.width.baseVal.value;
         var h1 = obj1.node.height.baseVal.value;
@@ -24,11 +40,11 @@
         var y2 = obj2.matrix.f;
         return "M"+(x1+(w1/2))+","+(y1+(h1/2))+"L"+(x2+(w2/2))+","+(y2+(h2/2));
     }
-    
+
     R.el.addPath = function(obj) {
         this.paths = this.paths || {};
         obj.paths = obj.paths || {};
-        
+
         if(this.paths[obj.id] == null) {
             var path = this.paper.path(getPathString(this, obj))
                 .attr({
@@ -36,19 +52,19 @@
                     'stroke-dasharray': '',
                     'stroke-width': 3
                 }).toBack();
-            
+
             this.paths[obj.id] = {
                 path: path,
                 obj: obj
             };
-            
+
             obj.paths[this.id] = {
                 path: path,
                 obj: this
             };
         }
     }
-    
+
     R.el.updatePaths = function() {
         for(var k in this.paths) {
             this.paths[k].path.attr(
@@ -58,7 +74,7 @@
             ).toBack();
         }
     }
-    
+
     R.el.updatePathDash = function(dash) {
         for(var k in this.paths) {
             var d = dash;
@@ -69,7 +85,7 @@
             ).toBack();
         }
     }
-    
+
     R.el.updatePathWidth = function(width) {
         for(var k in this.paths) {
             this.paths[k].path.attr(
@@ -79,24 +95,25 @@
             ).toBack();
         }
     }
-    
+
     R.el.init = function(x, y) {
         x = x || 0;
         y = y || 0;
         this.paths = {};
         this.drag(dragMove, dragStart, dragEnd);
         this.transform("T"+x+","+y);
+        this.original_transform = this.transform();
         return this;
     }
-    
+
     window.onload = function() {
         var paper = new R('canvas', '100%', '100%');
         var r1 = paper.rect(0, 0, 50, 50).attr({
             fill: '#F00'
         }).init(200, 200);
-        
+
         r1.id = "id" + r1.id;
-        
+
         var points = [
             {
                 x: 50,
@@ -113,7 +130,7 @@
             {
                 x: 350,
                 y: 50
-            },            
+            },
             {
                 x: 50,
                 y: 350
@@ -147,7 +164,7 @@
                 y: 250
             }
         ];
-        
+
         for (var i = 0; i < points.length; i++) {
             var sqr = paper.rect(0, 0, 50, 50).attr({
                 fill: '#F00'
@@ -155,7 +172,7 @@
             sqr.id = "id" + sqr.id;
             r1.addPath(sqr);
         }
-        
+
         var prev = null;
         var rad = document.getElementsByName('dashes');
         for(var i = 0; i < rad.length; i++) {
@@ -166,7 +183,7 @@
                 r1.updatePathDash(this.value);
             };
         }
-        
+
         var strokeSize = document.getElementsByName('strokeSize')[0];
         strokeSize.addEventListener('input', function() {
             strokeSize.value = (strokeSize.value < 16) ? strokeSize.value : 15;
@@ -174,5 +191,5 @@
             r1.updatePathWidth(strokeSize.value);
         });
     }
-    
+
 })(Raphael);
